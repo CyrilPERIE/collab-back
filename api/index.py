89c3528b687495cac9997b7e630113f8c00bb245
Cyril PERIE <cyril.perie96@gmail.com>
2024-04-18 13:50:12 +0200

    Allow cors

--- api/index.py
+++ api/index.py
@@ -1,14 +1,12 @@
 from flask import Flask
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin()
 def home():
     return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
 
 @app.route('/health-check')
 def health_check():
